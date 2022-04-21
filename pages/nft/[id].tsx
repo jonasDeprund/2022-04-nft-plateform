c34a066af6fb7b4232b6eb34457bd51a068eea1a
jonas <jonasdeprund@gmail.com>
2022-04-21 17:57:35 +0200

    add screen

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function NFTDropPage() {
   return (
-    <div>NFTDropPage</div>
+    <div className="flex h-screen flex-col">
+      {/* {Left part} */}
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+        <div className="w-44 rounded-xl object-cover lg:h-96 lg:w-72">
+          <img src="https://links.papareact.com/8sg" alt="" />
+          <div>
+            <h1>Jojo Apes</h1>
+            <h2>A collection of Apes</h2>
+          </div>
+        </div>
+      </div>
+
+      {/* {Right part} */}
+      <div></div>
+    </div>
   )
 }
 
