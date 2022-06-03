9c51cde706cba099acc9cbc2d19149f70c23d2cd
jonas <jonasdeprund@gmail.com>
2022-06-03 17:45:08 +0200

    node modules added

---
+++ sanity.js
@@ -0,0 +1,9 @@
+import { createCurrentUserHook } from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-08-11', // or today's date for latest
+  useCdn: process.env.NODE_ENV === 'production',
+}
